bc49fcf9dd069b8e4eca8eeb14ef1dd661fa0cdd
Thiago Figueiró <thiago.figueiro.ribeiro@gmail.com>
2026-08-22 20:55:13 -0300

    premiere 2 prepare

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 call conda activate esim
 
-python tools/premiere_video.py -i video.mp4 -o video_input
+python tools/prepare_video.py -i video.mp4 -o video_input
 python -m esim.cli --input video_input --output video_out --contrast-threshold 0.2
 python -m esim.event_frames video_out/events.txt --output video_out/event_frames --window-ms 10
